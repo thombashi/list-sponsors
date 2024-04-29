8f25006bf547fc704f7e9b008e83e34dd069b4d8
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2024-04-30 00:30:11 +0900

    Update project URLs in setup.py
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(exclude=["tests*"]),
     project_urls={
+        "Changelog": f"{REPOSITORY_URL:s}/releases",
         "Source": REPOSITORY_URL,
         "Tracker": f"{REPOSITORY_URL:s}/issues",
     },
